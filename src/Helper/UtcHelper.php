562454f6307a1afe07f8f9896722914ffed02b2d
Bob van de Vijver <bob@drenso.nl>
2020-11-05 14:14:29 +0100

    Move local timezone helper to UtcHelper class

--- src/Helper/UtcHelper.php
+++ src/Helper/UtcHelper.php
@@ -2,7 +2,11 @@
 
 namespace Drenso\Shared\Helper;
 
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
 use DateTimeZone;
+use RuntimeException;
 
 class UtcHelper
 {
@@ -18,5 +22,22 @@ class UtcHelper
   public static function getUtc(): DateTimeZone
   {
     return self::$utc ?: self::$utc = new DateTimeZone('UTC');
+  }
+
+  public static function convertToLocalTimezone(DateTimeInterface $dateTime): DateTimeInterface
+  {
+    $timezone = (new DateTime())->getTimezone();
+
+    if (PHP_MAJOR_VERSION >= 8) {
+      /** @noinspection PhpElementIsNotAvailableInCurrentPhpVersionInspection */
+      /** @phan-suppress-next-line PhanUndeclaredStaticMethod */
+      return DateTime::createFromInterface($dateTime)->setTimezone($timezone);
+    } else if ($dateTime instanceof DateTime) {
+      return (clone $dateTime)->setTimezone($timezone);
+    } else if ($dateTime instanceof DateTimeImmutable) {
+      return $dateTime->setTimezone($timezone);
+    }
+
+    throw new RuntimeException('Local timezone could not be set');
   }
 }
