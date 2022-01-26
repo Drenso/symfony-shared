7f1b81ca4d601fb5de5fabf5564f6a018d24bd4d
Tobias Feijten <tobias@drenso.nl>
2022-01-26 16:21:03 +0100

    Fix 'Typed property access before initialization' errors and remove legacy code

--- src/Helper/UtcHelper.php
+++ src/Helper/UtcHelper.php
@@ -3,16 +3,14 @@
 namespace Drenso\Shared\Helper;
 
 use DateTime;
-use DateTimeImmutable;
 use DateTimeInterface;
 use DateTimeZone;
-use RuntimeException;
 
 class UtcHelper
 {
-  private static \DateTimeZone $utc;
+  private static ?DateTimeZone $utc = null;
 
-  private static \DateTimeZone $local;
+  private static ?DateTimeZone $local = null;
 
   public static function getUtc(): DateTimeZone
   {
@@ -23,14 +21,6 @@ class UtcHelper
   {
     $timezone = self::$local ?: self::$local = new DateTimeZone(date_default_timezone_get());
 
-    if (PHP_MAJOR_VERSION >= 8) {
-      return DateTime::createFromInterface($dateTime)->setTimezone($timezone);
-    } elseif ($dateTime instanceof DateTime) {
-      return (clone $dateTime)->setTimezone($timezone);
-    } elseif ($dateTime instanceof DateTimeImmutable) {
-      return $dateTime->setTimezone($timezone);
-    }
-
-    throw new RuntimeException('Local timezone could not be set');
+    return DateTime::createFromInterface($dateTime)->setTimezone($timezone);
   }
 }
