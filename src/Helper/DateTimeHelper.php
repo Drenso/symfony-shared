dfaf75e02206f7905ddfb4e0e7235923b01c659d
Bob van de Vijver <bob@drenso.nl>
2022-06-29 11:58:25 +0200

    Deprecate UtcHelper in favour of new DateTimeHelper

---
+++ src/Helper/DateTimeHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drenso\Shared\Helper;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+
+class DateTimeHelper
+{
+  private static ?DateTimeZone $utc = null;
+
+  private static ?DateTimeZone $local = null;
+
+  public static function getUtcTimeZone(): DateTimeZone
+  {
+    return self::$utc ?: self::$utc = new DateTimeZone('UTC');
+  }
+
+  public static function getLocalTimeZone(): DateTimeZone
+  {
+    return self::$local ?: self::$local = new DateTimeZone(date_default_timezone_get());
+  }
+
+  public static function convertTo(
+      DateTimeInterface $toConvert,
+      ?DateTimeZone $timezone = null): DateTime
+  {
+    $converted = DateTime::createFromInterface($toConvert);
+
+    return $timezone === null ? $converted : $converted->setTimezone($timezone);
+  }
+
+  public static function convertToImmutable(
+      DateTimeInterface $toConvert,
+      ?DateTimeZone $timezone = null): DateTimeImmutable
+  {
+    $converted = DateTimeImmutable::createFromInterface($toConvert);
+
+    return $timezone === null ? $converted : $converted->setTimezone($timezone);
+  }
+
+  public static function convertToLocal(DateTimeInterface $toConvert): DateTime
+  {
+    return self::convertTo($toConvert, self::getLocalTimeZone());
+  }
+
+  public static function convertToLocalImmutable(DateTimeInterface $toConvert): DateTimeImmutable
+  {
+    return self::convertToImmutable($toConvert, self::getLocalTimeZone());
+  }
+
+  public static function convertToUtc(DateTimeInterface $toConvert): DateTime
+  {
+    return self::convertTo($toConvert, self::getUtcTimeZone());
+  }
+
+  public static function convertToUtcImmutable(DateTimeInterface $toConvert): DateTimeImmutable
+  {
+    return self::convertToImmutable($toConvert, self::getUtcTimeZone());
+  }
+}
--- src/Helper/UtcHelper.php
+++ src/Helper/UtcHelper.php
@@ -2,72 +2,31 @@
 
 namespace Drenso\Shared\Helper;
 
-use DateTime;
-use DateTimeImmutable;
 use DateTimeInterface;
 use DateTimeZone;
 
+/**
+ * @deprecated Replaced by a new class
+ * @see        DateTimeHelper
+ */
 class UtcHelper
 {
-  private static ?DateTimeZone $utc = null;
-
-  private static ?DateTimeZone $local = null;
-
   public static function getUtc(): DateTimeZone
   {
-    return self::$utc ?: self::$utc = new DateTimeZone('UTC');
+    return DateTimeHelper::getUtcTimeZone();
   }
 
   public static function getLocal(): DateTimeZone
   {
-    return self::$local ?: self::$local = new DateTimeZone(date_default_timezone_get());
+    return DateTimeHelper::getLocalTimeZone();
   }
 
-  /** @deprecated You should switch to the strictly typed alternatives */
   public static function convertToLocalTimezone(
       DateTimeInterface $dateTime,
       bool $asImmutable = false): DateTimeInterface
   {
     return $asImmutable
-        ? self::convertToLocalImmutable($dateTime)
-        : self::convertToLocal($dateTime);
-  }
-
-  public static function convertTo(
-      DateTimeInterface $toConvert,
-      ?DateTimeZone $timezone = null): DateTime
-  {
-    $converted = DateTime::createFromInterface($toConvert);
-
-    return $timezone === null ? $converted : $converted->setTimezone($timezone);
-  }
-
-  public static function convertToImmutable(
-      DateTimeInterface $toConvert,
-      ?DateTimeZone $timezone = null): DateTimeImmutable
-  {
-    $converted = DateTimeImmutable::createFromInterface($toConvert);
-
-    return $timezone === null ? $converted : $converted->setTimezone($timezone);
-  }
-
-  public static function convertToLocal(DateTimeInterface $toConvert): DateTime
-  {
-    return self::convertTo($toConvert, self::getLocal());
-  }
-
-  public static function convertToLocalImmutable(DateTimeInterface $toConvert): DateTimeImmutable
-  {
-    return self::convertToImmutable($toConvert, self::getLocal());
-  }
-
-  public static function convertToUtc(DateTimeInterface $toConvert): DateTime
-  {
-    return self::convertTo($toConvert, self::getUtc());
-  }
-
-  public static function convertToUtcImmutable(DateTimeInterface $toConvert): DateTimeImmutable
-  {
-    return self::convertToImmutable($toConvert, self::getUtc());
+        ? DateTimeHelper::convertToLocalImmutable($dateTime)
+        : DateTimeHelper::convertToLocal($dateTime);
   }
 }
