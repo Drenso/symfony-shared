608ac45b9ccf0cc559a4b819f759a758cfe9fa49
Bob van de Vijver <bob@drenso.nl>
2022-07-28 09:57:41 +0200

    Add @return types to fix deprecation notices

--- src/Database/Types/PhpDecimalType.php
+++ src/Database/Types/PhpDecimalType.php
@@ -10,12 +10,21 @@ class PhpDecimalType extends StringType
 {
   final public const NAME = 'php_decimal';
 
-  /** @param Decimal|null $value */
+  /**
+   * @param Decimal|null $value
+   *
+   * @return mixed|string|null
+   */
   public function convertToDatabaseValue($value, AbstractPlatform $platform)
   {
     return $value?->toString();
   }
 
+  /**
+   * @param mixed $value
+   *
+   * @return mixed|Decimal|null
+   */
   public function convertToPHPValue($value, AbstractPlatform $platform): ?Decimal
   {
     if ($value === null || $value === '') {
