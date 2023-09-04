d1a14faef56cc477162e986c27af13ac17638bb6
Bob van de Vijver <bob@drenso.nl>
2023-09-04 11:51:32 +0200

    Add InMemoryCacheTrait to easy in memory result caching

---
+++ src/MemoryCache/InMemoryCacheTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drenso\Shared\MemoryCache;
+
+use Closure;
+
+trait InMemoryCacheTrait
+{
+  private array $_memCache = [];
+
+  /**
+   * @template T
+   *
+   * @param Closure():T $builder
+   *
+   * @return T
+   */
+  protected function memCachedResult(string $key, Closure $builder): mixed
+  {
+    return $this->_memCache[$key] ??= $builder();
+  }
+}
