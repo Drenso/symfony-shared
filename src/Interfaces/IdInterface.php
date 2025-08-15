3afdab49cdd8fffa50c9b69616d3040311328278
Bob van de Vijver <bob@drenso.nl>
2025-08-15 10:35:02 +0200

    Drop positive requirement on id

--- src/Interfaces/IdInterface.php
+++ src/Interfaces/IdInterface.php
@@ -7,10 +7,6 @@ interface IdInterface
   /** Retrieve the object id which can be null in case the object hasn't been persisted yet. */
   public function getId(): ?int;
 
-  /**
-   * Retrieve the object id which is confirmed to be non-null and positive.
-   *
-   * @return positive-int
-   */
+  /** Retrieve the object id which is confirmed to be non-null. */
   public function getNonNullId(): int;
 }
