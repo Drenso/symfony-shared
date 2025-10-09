d6bee0654157445f9ccde48d1053d34d624b9d0c
Bob van de Vijver <bob@drenso.nl>
2025-10-09 12:26:48 +0200

    Add Ulid support to NotFoundException

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -3,11 +3,14 @@
 namespace Drenso\Shared\Exception;
 
 use RuntimeException;
+use Symfony\Component\Uid\Ulid;
 
 class NotFoundException extends RuntimeException
 {
-  public function __construct(string $type, int $id)
+  public function __construct(string $type, int|Ulid $id)
   {
-    parent::__construct(sprintf('Could not find %s with id %d', $type, $id));
+    $idString = $id instanceof Ulid ? $id->toBase32() : sprintf('%d', $id);
+
+    parent::__construct("Could not find $type with id $idString");
   }
 }
